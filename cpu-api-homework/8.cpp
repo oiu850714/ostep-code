3efddebe8b0f509483f78f51ea650f571f7ed3bc
oiu850714 <oiu850714@gmail.com>
2020-03-30 22:19:33 +0800

    add incomplete solution for ch5.8

---
+++ cpu-api-homework/8.cpp
@@ -0,0 +1,49 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <cstdlib>
+#include <iostream>
+
+int main() {
+    // first child sends to second child
+    int pipefd[2] = {};
+    if (pipe(pipefd) == -1) {
+        std::cerr << "create pipe failed" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    auto pidfirst = fork();
+    if (pidfirst < 0) {
+        std::cerr << "Failed to fork first child" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    if (pidfirst == 0) {
+        close(pipefd[0]);
+        std::string s = "Wrtie to second child";
+        write(pipefd[1], s.c_str(), s.size());
+        close(pipefd[1]);
+        std::cout << "ㄅㄅ1" << std::endl;
+        exit(EXIT_SUCCESS);
+    }
+
+    auto pidsecond = fork();
+    if (pidsecond < 0) {
+        std::cerr << "Failed to fork second child" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    if (pidsecond == 0) {
+        close(pipefd[1]);
+        char buf;
+        while (read(pipefd[0], &buf, sizeof(buf)) > 0) {
+            write(STDOUT_FILENO, &buf, sizeof(buf));
+        }
+        close(pipefd[0]);
+        std::cout << "ㄅㄅ2" << std::endl;
+        exit(EXIT_SUCCESS);
+    }
+
+    waitpid(pidfirst, NULL, 0);
+    waitpid(pidsecond, NULL, 0); // stock
+    std::cout << "first: " << pidfirst << "second: " << pidsecond << std::endl;
+}
