86775a362c82d17ba92f892371ed4a68a0346753
oiu850714 <oiu850714@gmail.com>
2020-04-02 07:26:08 +0800

    add solution for ch6's system call section

---
+++ exercises/ch6/measure_syscall.cpp
@@ -0,0 +1,20 @@
+#include <sys/time.h> // gettimeofday
+#include <unistd.h> // read
+#include <vector> // std::vector
+#include <utility> // std::pair
+#include <iostream> // std::cout
+
+double getEstimatedSyscallTime(const timeval &start, const timeval &end, const int iteration) {
+    return ((1000000 * end.tv_sec + end.tv_usec) - (1000000 * start.tv_sec + start.tv_usec)) / iteration;
+}
+
+int main() {
+    timeval start{}, end{};
+    constexpr int iteration = 1000000;
+    gettimeofday(&start, nullptr);
+    for (int i = 0; i < iteration; i++) {
+        read(STDIN_FILENO, nullptr, 0);
+    }
+    gettimeofday(&end, nullptr);
+    std::cout << "Estimated syscall overhead: " << getEstimatedSyscallTime(start, end, iteration) << "us" << std::endl;
+}
